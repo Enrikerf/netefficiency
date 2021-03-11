43c4c97bb07e18d872e99d0a5159518d0f93a516
Enrique Arrabal <enrike9066@gmail.com>
2021-03-11 21:03:10 +0100

    symfony installed

--- public/index.php
+++ public/index.php
@@ -1,15 +1,22 @@
 <?php
-include('versionb/All_classes/Main_finance.php');
 
-if ($_SERVER['REQUEST_URI'] === '/time') {
-    $data = new Main_finance();
-    echo $data->today();
-} elseif ($_SERVER['REQUEST_URI'] === '/vat') {
-	$tax = 20;
-	$price = 400;
-	$data = new Main_finance();
-    echo ' VAT: ' . $data->VAT($price, $tax);
-    echo ' Tax: ' . $tax;
-} else {
-    echo 'Hello!';
+use App\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\ErrorHandler\Debug;
+use Symfony\Component\HttpFoundation\Request;
+
+require dirname(__DIR__).'/vendor/autoload.php';
+
+(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+
+if ($_SERVER['APP_DEBUG']) {
+    umask(0000);
+
+    Debug::enable();
 }
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
